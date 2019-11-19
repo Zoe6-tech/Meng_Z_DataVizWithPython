e415341e67d3164b1c0cff29f79c9122873452fd
Meng <z_meng@ats-hr420-dm05.fc.ca>
2019-11-19 14:18:13 -0500

    adding OlympicHockeyMedals

---
+++ data/basicpie.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+# Pie chart
+hours=[4,8,2]
+activities=['sleep','work','play']
+colors=['gold','silver','blue']
+
+plt.pie(hours,labels=activities,
+	colors=colors,startangle=45,
+	autopct='%.1f%%')
+
+plt.show()
